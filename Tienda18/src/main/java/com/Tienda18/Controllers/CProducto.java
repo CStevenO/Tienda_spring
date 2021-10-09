e2e10141f87f8136a97908ef504d6b798a925e04
Steven <53093183+CStevenO@users.noreply.github.com>
2021-10-09 11:48:29 -0500

    Conexion con base de datos AWS

--- Tienda18/src/main/java/com/Tienda18/Controllers/CProducto.java
+++ Tienda18/src/main/java/com/Tienda18/Controllers/CProducto.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -113,6 +114,11 @@ public class CProducto {
 		boolean eliminado = productosS.eliminar(id);
 		if(eliminado) return "Producto Eliminado";
 		else return "Error Eliminado Producto";
+	}
+	
+	@PostMapping(path = "/agregar")
+	public MProducto guardar(@RequestBody MProducto producto){
+		return productosS.guardar(producto);
 	}
 	
 }
